de05587af95e43c500cfb6abf1fea31664381740
Shelbot77 <alessiodeangelis021@gmail.com>
2023-01-11 14:35:13 +0100

    repo setup commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+
+
+export default {
+    name: 'AppHeader'
+
+}
+</script>
+
+<template>
+    <h1>Questo è l'header</h1>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,18 @@
+<script>
+
+
+export default {
+    name: 'AppMain'
+
+}
+</script>
+
+<template>
+    <h2>Questo è il main</h2>
+
+
+</template>
+
+<style scoped>
+
+</style>
